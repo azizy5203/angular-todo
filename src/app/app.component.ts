da7d7a5c10b1eb299a9e95f9bbed8b953be2440a
Youssef Abdulaziz <azizy5203@gmail.com>
2025-03-24 09:49:49 +0200

    feat : main layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppSidebarComponent } from "./app-sidebar/app-sidebar.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppHeaderComponent],
+  imports: [RouterOutlet, AppHeaderComponent, AppSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
