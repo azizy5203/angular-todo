9ce1f35bf547f5294fa06eeb9949be93dc14a380
Youssef Abdulaziz <azizy5203@gmail.com>
2025-03-25 11:57:49 +0200

    feat : Added grid stack setup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { GridStackOptions, GridStackWidget } from 'gridstack';
+import { GridstackComponent, GridstackItemComponent } from 'gridstack/dist/angular';
 
 @Component({
   selector: 'home',
-  imports: [],
+  imports: [GridstackComponent,
+    GridstackItemComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent { }
+export class HomeComponent {
+  public gridOptions: GridStackOptions = {
+    margin: 20,
+    column: 5,
+    row: 1,
+    alwaysShowResizeHandle: true,
+    resizable: {
+      // autoHide: false,
+      handles: 'se, sw'
+    },
+    animate: true, handle: ".grid-stack-item"
+  };
+  public items: GridStackWidget[] = [
+    { x: 0, y: 0, id: '1' }, // must have unique id used for trackBy
+    { x: 1, y: 0, id: '2' },
+    { x: 0, y: 1, id: '3' },
+  ];
+}
