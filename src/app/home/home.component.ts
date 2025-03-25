aff94b77b3ebf2a2ffd938341a17a85437159071
Youssef Abdulaziz <azizy5203@gmail.com>
2025-03-25 13:09:38 +0200

    tweak : added items and adjusted layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,9 +11,9 @@ import { GridstackComponent, GridstackItemComponent } from 'gridstack/dist/angul
 })
 export class HomeComponent {
   public gridOptions: GridStackOptions = {
-    margin: 20,
+    margin: 10,
     column: 5,
-    row: 1,
+    row: 2,
     alwaysShowResizeHandle: true,
     resizable: {
       // autoHide: false,
@@ -22,8 +22,10 @@ export class HomeComponent {
     animate: true, handle: ".grid-stack-item"
   };
   public items: GridStackWidget[] = [
-    { x: 0, y: 0, id: '1' }, // must have unique id used for trackBy
-    { x: 1, y: 0, id: '2' },
-    { x: 0, y: 1, id: '3' },
+    { x: 0, y: 0, id: '1',h:2 }, // must have unique id used for trackBy
+    { x: 1, y: 0, id: '2',w:3 },
+    { x: 4, y: 0, id: '3' },
+    { x: 1, y: 1, id: '4' },
+    { x: 2, y: 1, id: '5',w:3 },
   ];
 }
